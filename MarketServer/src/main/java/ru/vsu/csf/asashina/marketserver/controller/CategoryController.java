67d1892d18aba2b96b8f42a9648ee5e47b2a5e3e
Arcturs <yaninastya2010@yandex.ru>
2023-01-31 00:36:31 +0300

    adding description to all categories endpoints

--- MarketServer/src/main/java/ru/vsu/csf/asashina/marketserver/controller/CategoryController.java
+++ MarketServer/src/main/java/ru/vsu/csf/asashina/marketserver/controller/CategoryController.java
@@ -1,15 +1,18 @@
 package ru.vsu.csf.asashina.marketserver.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.vsu.csf.asashina.marketserver.model.ResponseBuilder;
-import ru.vsu.csf.asashina.marketserver.model.dto.CategoryDTO;
-import ru.vsu.csf.asashina.marketserver.model.dto.CategoryWithProductsDTO;
-import ru.vsu.csf.asashina.marketserver.model.dto.PagingInfoDTO;
-import ru.vsu.csf.asashina.marketserver.model.dto.ProductDTO;
+import ru.vsu.csf.asashina.marketserver.model.dto.*;
 import ru.vsu.csf.asashina.marketserver.model.request.CategoryCreateRequest;
 import ru.vsu.csf.asashina.marketserver.model.request.ProductsListToAttachToCategoryRequest;
 import ru.vsu.csf.asashina.marketserver.service.CategoryService;
@@ -22,15 +25,34 @@ import static org.springframework.http.HttpStatus.*;
 @RequestMapping("/categories")
 public class CategoryController {
 
+    private final static String CATEGORY_TAG = "Category";
+
     private final CategoryService categoryService;
     private final ProductService productService;
 
     @GetMapping("")
+    @Operation(summary = "Gets all categories in list", tags = CATEGORY_TAG, responses = {
+            @ApiResponse(responseCode = "200", description = "List of categories returned", content = {
+                    @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = CategoryDTO.class)))
+            })
+    })
     public ResponseEntity<?> getAllCategoriesInList() {
         return ResponseBuilder.build(OK, categoryService.getAllCategories());
     }
 
     @GetMapping("/{id}/products")
+    @Operation(summary = "Fetches all products in specifies category", tags = CATEGORY_TAG, responses = {
+            @ApiResponse(responseCode = "200", description = "Returns products in pages with category information", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryWithProductsDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Category does not exist", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            })
+    })
     public ResponseEntity<?> getAllProductsInCategoryInPagesById(@PathVariable("id") Long id,
                                                           @RequestParam(value = "pageNumber", required = false, defaultValue = "1") Integer pageNumber,
                                                           @RequestParam(value = "size", required = false, defaultValue = "5") Integer size,
@@ -52,11 +74,31 @@ public class CategoryController {
     }
 
     @PostMapping("")
+    @Operation(summary = "Creates new category", tags = CATEGORY_TAG, responses = {
+            @ApiResponse(responseCode = "201", description = "Returns new category", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Category with following name already exists", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            })
+    })
     public ResponseEntity<?> createCategoryFromRequest(@RequestBody @Valid CategoryCreateRequest request) {
         return ResponseBuilder.build(CREATED, categoryService.createCategoryFromCreateRequest(request));
     }
 
     @PostMapping("/{id}/attach-products")
+    @Operation(summary = "Attach products to specified category", tags = CATEGORY_TAG, responses = {
+            @ApiResponse(responseCode = "200", description = "Products were successfully attached"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Category/Products do not exist", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            })
+    })
     public ResponseEntity<?> attachProductsToCategory(@PathVariable("id") Long id,
                                                       @RequestBody @Valid ProductsListToAttachToCategoryRequest request) {
         CategoryDTO category = categoryService.getCategoryById(id);
@@ -65,6 +107,15 @@ public class CategoryController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes category", tags = CATEGORY_TAG, responses = {
+            @ApiResponse(responseCode = "204", description = "Category was deleted"),
+            @ApiResponse(responseCode = "400", description = "Invalid category id", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Category does not exist", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class))
+            })
+    })
     public ResponseEntity<?> deleteCategoryById(@PathVariable("id") Long id) {
         categoryService.deleteCategoryById(id);
         return ResponseBuilder.buildWithoutBodyResponse(NO_CONTENT);
